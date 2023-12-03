fae87bc00e2d67bfe9c082c3351cb7177af188f9
rodionion <rodionvh@gmail.com>
2023-12-03 11:56:14 -0300

    Add welcome section to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,7 @@
 import "../styles/home.css"
+import womenWithBaskets from "../resources/women-with-baskets.jpg";
+import womanHoldingNuts from "../resources/woman-holding-nuts.jpg";
+import basketWithSeeds from "../resources/basket-with-seeds.jpg";
 
 export default function Home() {
     return (
@@ -15,7 +18,18 @@ export default function Home() {
                 </button>
             </section>
             <section className="welcomeSection">
-                <h1>Welcome Section</h1>
+                <h1>Welcome to Kalahari Natural Oils!</h1>
+                <div className="contentContainer">
+                    <p className="text1">
+                        For more than 2 decades we have been producing 100% organic and pure natural African oils. We make sure that your ingredients are freshly made and of best quality. Our oils are extracted to order and with integrity. 
+                    </p>
+                    <img className="womenWithBasketsImage image" src={womenWithBaskets} alt="" />
+                    <img className="womanHoldingNutsImage image" src={womanHoldingNuts} alt="" />
+                    <p className="text2">
+                        Kalahari Natural Oils Ltd was founded in 2006. We work with communities in remote rural parts of Zambia. The Wild collection from the forests is mainly done by women. We follow Organic Production and Processing Rules and UN BioTrade Pinciples & Criteria for use of Terrestrial Biodiversity.
+                    </p>
+                    <img className="basketWithSeedsImage image" src={basketWithSeeds} alt="" />
+                </div>
             </section>
             <section className="whyUseSection">
                 <h1>Why Use Section</h1>
